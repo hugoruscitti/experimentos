ee8f8ddc5081b238532735025e05d8a48be79259
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-09-25 14:21:23 -0300

    Creando metodo post en la aplicacion para publicar mensajes en redis.

--- realtime_abm/app.py
+++ realtime_abm/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, request
 from flask.ext.bootstrap import Bootstrap
+import redis
+import datetime
 
 app = Flask(__name__)
+app.secret_key = 'asdf'
+red = redis.StrictRedis()
 Bootstrap(app)
 
 @app.route("/")
@@ -15,6 +19,28 @@ def grafico():
 @app.route("/chat")
 def chat():
     return render_template('chat.html')
+
+def event_stream():
+    pubsub = red.pubsub()
+    pubsub.subscribe('chat')
+    for message in pubsub.listen():
+        print message
+        yield 'data: %s\n\n' % message['data']
+
+@app.route('/enviar', methods=['POST'])
+def enviar():
+    message = request.form['m']
+    user = "anonimo"
+    now = datetime.datetime.now().replace(microsecond=0).time()
+    red.publish('chat', u'[%s] %s: %s' % (now.isoformat(), user, message))
+    print now, message
+    return {}
+
+
+
+@app.route('/stream')
+def stream():
+    return Response(event_stream(), mimetype="text/event-stream")
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
