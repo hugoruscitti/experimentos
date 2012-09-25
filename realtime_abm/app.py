7c6f54a4f8ed93cb329e0795674460958e7426db
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-09-25 12:16:26 -0300

    Comenzando a crear el ejemplo de chat.

--- realtime_abm/app.py
+++ realtime_abm/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 
-
 app = Flask(__name__)
 Bootstrap(app)
 
@@ -9,6 +8,13 @@ Bootstrap(app)
 def home():
     return render_template('base.html')
 
+@app.route("/grafico")
+def grafico():
+    return render_template('grafico.html')
+
+@app.route("/chat")
+def chat():
+    return render_template('chat.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
