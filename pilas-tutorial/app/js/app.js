80972726ab78f9930017ad89d8e63915c98bf0d8
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-09-21 19:03:41 -0300

    Reestructurando la estructura de las vistas.

--- pilas-tutorial/app/js/app.js
+++ pilas-tutorial/app/js/app.js
@@ -2,26 +2,68 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-  	when('/', {templateUrl: 'partials/index.html'}).
-  	when('/numeros', {templateUrl: 'partials/numeros.html'}).
+  	when('/',  {templateUrl: 'partials/paso1.html', controller: 'Paso1Ctrl'}).
+  	when('/2', {templateUrl: 'partials/paso2.html', controller: 'Paso2Ctrl'}).
+  	when('/3', {templateUrl: 'partials/paso3.html', controller: 'Paso3Ctrl'}).
+  	when('/4', {templateUrl: 'partials/paso4.html', controller: 'Paso4Ctrl'}).
+  	when('/5', {templateUrl: 'partials/paso5.html', controller: 'Paso5Ctrl'}).
   	otherwise({redirectTo: '/'});
 }]);
 
 
 
-app.controller('TutorialCtrl', ['$scope', function($scope) {
+app.controller('TutorialCtrl', ['$scope', '$location', function($scope, $location) {
+  $scope.mensaje = "";
   
-  $scope.datos = {
-  	nombre: 'Hugo Ruscitti',
-  };
+  $scope.ocultar_mensajes = function() {
+    $scope.mensaje = "";
+  }
   
-  $scope.exito = false;
+  function es_actual() {
+    var path_del_ejercicio = "/" + this.path;
+    var path_actual = $location.path();
+    return (path_actual == path_del_ejercicio);
+  }
   
-  $scope.cuando_ejecuta = function(dato) {
-    if (dato == 4)
-      $scope.exito = true;
-    else
-      alert("sigue intentando...");
+  $scope.ejercicios = [
+    {numero: 1, completado: false, path:  '', es_actual: es_actual},
+    {numero: 2, completado: false, path: '2', es_actual: es_actual},
+    {numero: 3, completado: false, path: '3', es_actual: es_actual},
+    {numero: 4, completado: false, path: '4', es_actual: es_actual},
+    {numero: 5, completado: false, path: '5', es_actual: es_actual},
+  ];
+    
+   /* La funcion "cuando_ejecuta" contiene el callback que se va
+      a llamar cada vez que el usuario escriba dentro de la consola.
+      
+      El argumento que recibe es la salida por pantalla (en formato string).
+      
+      Este callback va a ser sobre-escrito por cada controller. Así cada
+      consigna del tutorial hacer sus propias validaciones y flujo de
+      programa.
+    */
+   $scope.cuando_ejecuta = function(datos) {
+   };
+    
+}]);
+
+app.controller('Paso1Ctrl', ['$scope', '$location', function($scope, $location) {
+  
+  $scope.$parent.cuando_ejecuta = function(dato) {
+    if (dato == 4) {
+      $location.path('/2');
+      $scope.$parent.mensaje = "Ejercicio 1 completado!";
+    } 
+  }
+  
+}]);
+
+app.controller('Paso2Ctrl', ['$scope', '$location', function($scope, $location) {
+  $scope.$parent.cuando_ejecuta = function(data) {
+   
+    if (window['nombre']) {
+      $scope.$parent.nombre = window.nombre;
+    }
   }
   
 }]);
@@ -34,18 +76,13 @@ app.directive('pilasInterprete', function() {
     transclude: true,
     link: function (scope, elem, attrs) {
     
-      //elem[0].addEventListener('entrada', function (e) {console.log(e); alert('el usuaior ingreso: ' + e.texto)}, false);
-      
       /* Se conecta al evento de impresion de pantalla que emite la consola.
          Cuando llega esta senal intenta conectarla con el callback de la
          directiva.
          
-         Todo esto se evalua en el contexto del scope.
-      */
+         Todo esto se evalua en el contexto del scope. */
       elem[0].addEventListener('salida', function (e) {
-        var funcion = attrs.evaluador.replace("()", "(" + e.texto + ")");
-        
-        console.log(funcion);
+        var funcion = attrs.evaluador.replace("()", "('" + e.texto + "')");
       	scope.$eval(funcion);
         scope.$apply();
       }, false);
