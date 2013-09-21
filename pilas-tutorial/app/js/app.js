fb51961a41f23250b78802f8ec877c2b7c179456
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-09-21 02:02:00 -0300

    Creando la directiva pilas-interprete con un evaluador minimo.

--- pilas-tutorial/app/js/app.js
+++ pilas-tutorial/app/js/app.js
@@ -7,10 +7,58 @@ app.config(['$routeProvider', function($routeProvider) {
   	otherwise({redirectTo: '/'});
 }]);
 
+
+
 app.controller('TutorialCtrl', ['$scope', function($scope) {
   
   $scope.datos = {
   	nombre: 'Hugo Ruscitti',
   };
   
+  $scope.exito = false;
+  
+  $scope.cuando_ejecuta = function(dato) {
+    if (dato == 4)
+      $scope.exito = true;
+    else
+      alert("sigue intentando...");
+  }
+  
 }]);
+
+
+app.directive('pilasInterprete', function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    transclude: true,
+    link: function (scope, elem, attrs) {
+    
+      //elem[0].addEventListener('entrada', function (e) {console.log(e); alert('el usuaior ingreso: ' + e.texto)}, false);
+      
+      /* Se conecta al evento de impresion de pantalla que emite la consola.
+         Cuando llega esta senal intenta conectarla con el callback de la
+         directiva.
+         
+         Todo esto se evalua en el contexto del scope.
+      */
+      elem[0].addEventListener('salida', function (e) {
+        var funcion = attrs.evaluador.replace("()", "(" + e.texto + ")");
+        
+        console.log(funcion);
+      	scope.$eval(funcion);
+        scope.$apply();
+      }, false);
+      
+      iniciar_consola();
+    },
+    template: '<div id="consola" class="stretch console">' + 
+       	'<div id="console" class="stretch">' +
+        ' <ul id="output"></ul>' +
+        '  <form>' + 
+        '   <textarea autofocus id="exec" spellcheck="false" autocapitalize="off" rows="1" autocorrect="off"></textarea>' +
+        '  </form>' +
+      	' </div>' +
+        '</div>',
+  }
+});
